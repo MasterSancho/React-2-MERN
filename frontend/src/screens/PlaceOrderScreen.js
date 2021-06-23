d002c5bbd4184574d4129896d77765a48291a5af
Alex M <1maltsevalex@gmail.com>
2021-06-23 10:01:11 +0300

    59. Order Screen

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -59,47 +59,49 @@ const PlaceOrderScreen = ({ history }) => {
    <CheckoutSteps step1 step2 step3 step4 />
    <Row>
     <Col md={8}>
-     <ListGroup.Item>
-      <h2>Shipping</h2>
-      <p>
-       <strong>Address:</strong>
-       {cart.shippingAddress.address}, {cart.shippingAddress.city}{' '}
-       {cart.shippingAddress.postalCode}, {cart.shippingAddress.country}
-      </p>
-     </ListGroup.Item>
+     <ListGroup variant='flush'>
+      <ListGroup.Item>
+       <h2>Shipping</h2>
+       <p>
+        <strong>Address:</strong>
+        {cart.shippingAddress.address}, {cart.shippingAddress.city}{' '}
+        {cart.shippingAddress.postalCode}, {cart.shippingAddress.country}
+       </p>
+      </ListGroup.Item>
 
-     <ListGroup.Item>
-      <h2>Payment Method</h2>
-      <strong>Method: </strong>
-      {cart.paymentMethod}
-     </ListGroup.Item>
+      <ListGroup.Item>
+       <h2>Payment Method</h2>
+       <strong>Method: </strong>
+       {cart.paymentMethod}
+      </ListGroup.Item>
 
-     <ListGroup.Item>
-      <h2>Order Items</h2>
-      {cart.cartItems.length === 0 ? (
-       <Message>Your cart is empty</Message>
-      ) : (
-       <ListGroup variant='flush'>
-        {cart.cartItems.map((item, index) => (
-         <ListGroup.Item key={index}>
-          <Row>
-           <Col md={2}>
-            <Image src={item.image} alt={item.name} fluid rounded />
-           </Col>
+      <ListGroup.Item>
+       <h2>Order Items</h2>
+       {cart.cartItems.length === 0 ? (
+        <Message>Your cart is empty</Message>
+       ) : (
+        <ListGroup variant='flush'>
+         {cart.cartItems.map((item, index) => (
+          <ListGroup.Item key={index}>
+           <Row>
+            <Col md={2}>
+             <Image src={item.image} alt={item.name} fluid rounded />
+            </Col>
 
-           <Col>
-            <Link to={`/product/${item.product}`}>{item.name}</Link>
-           </Col>
+            <Col>
+             <Link to={`/product/${item.product}`}>{item.name}</Link>
+            </Col>
 
-           <Col md={4}>
-            {item.qty} x ${item.price} = ${item.qty * item.price}
-           </Col>
-          </Row>
-         </ListGroup.Item>
-        ))}
-       </ListGroup>
-      )}
-     </ListGroup.Item>
+            <Col md={4}>
+             {item.qty} x ${item.price} = ${item.qty * item.price}
+            </Col>
+           </Row>
+          </ListGroup.Item>
+         ))}
+        </ListGroup>
+       )}
+      </ListGroup.Item>
+     </ListGroup>
     </Col>
 
     <Col md={4}>
